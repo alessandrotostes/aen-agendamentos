69c22751d54d0a79c477772bfe3ae70f5d161850
alessandrotostes <laessandro.jr@gmail.com>
2025-09-17 16:50:45 -0300

    Configura integração com Sentry

---
+++ app-agendamento/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://07ebcf4e0b7409e0fe0598197ea76c46@o4510036636925952.ingest.us.sentry.io/4510036639285248",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
